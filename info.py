a7205049d07d84716edb09b2544edd47b376643f
imdbtelevision <161870953+imdbtelevision@users.noreply.github.com>
2024-03-31 17:53:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,8 +48,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://imdbtelevision:imdbtv51477@cluster0.9yjs6ml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "imdbtelevision")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://imdbtelevision51477:imdbtv51477@cluster0.9gt0q0q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "imdbtelevision51477")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
