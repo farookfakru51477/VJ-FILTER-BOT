324434f9fdd4a54cd62baa8d8f50bc0cbf252a0e
imdbtelevision <161870953+imdbtelevision@users.noreply.github.com>
2024-08-09 08:19:35 +0400

    Update info.py

--- info.py
+++ info.py
@@ -53,9 +53,9 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "imdbtelevision51477")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
+VERIFY = bool(environ.get('VERIFY', True))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'modijiurl.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '41337fa84024d00dcf682c9d15ddb9b34b15ac07')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
